bf0975436fc49638f098ec219a26902360727d17
valerhic <valerua516@gmail.com>
2024-07-11 17:29:42 +0300

    init

---
+++ ko.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function(){
+    output.innerHTML = 'Я твой рот ебал';
+});
+ 
